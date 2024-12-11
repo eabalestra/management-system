88404ac0d7d1be54c804f19a8c5fefe649575b1c
eabalestra <agusbalestra0@gmail.com>
2024-12-11 17:57:46 -0300

    add pg_search gem

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,10 @@
 class Product < ApplicationRecord
+  include PgSearch::Model
+  pg_search_scope :search_full_text, against: {
+    name: 'A',
+    description: 'B'
+  }
+
   has_one_attached :image_url
 
   belongs_to :supplier
